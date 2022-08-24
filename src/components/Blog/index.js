3af93d150485c7453a305bc3fc2718c0c530c4d2
antoineflg35 <antoineflegeau35@gmail.com>
2022-08-24 11:02:51 +0200

    Mise en place de requête AJAX via Axios + utilisation du useEffect pour envoyer les données posts après le rendu initial

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,25 +1,59 @@
 /* eslint-disable react/jsx-no-bind */
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import axios from 'axios';
 
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import filterPostsByCategory from 'src/components/selectors/filterPostsByCategory';
 
 // Composants
 import Header from 'src/components/Header';
 import Posts from 'src/components/Posts';
 import Footer from 'src/components/Footer';
+import NotFound from 'src/components/NotFound';
+import Spinner from 'src/components/Spinner';
+
 // data, styles et utilitaires
 import categoriesData from 'src/data/categories';
 import postsData from 'src/data/posts';
-import filterPostByCategory from '../Selector/filterPostsByCategory';
 import './styles.scss';
 
 // == Composant
 function Blog() {
   const [zenMode, setZenMode] = useState(false);
+
+  const [posts, setPosts] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
   function toggleZenMode() {
     setZenMode(!zenMode);
+
+    console.log('zenMode', zenMode);
   }
+
+  function loadPosts() {
+    setLoading(true);
+    axios
+      .get('https://oclock-open-apis.vercel.app/api/blog/posts')
+      // quand la promesse est tenue (que l'api nous a répondu et fourni les données)
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch(() => {
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
   const className = zenMode ? 'blog blog--zen' : 'blog';
+
+  useEffect(
+    loadPosts,
+    [],
+  );
+  
   return (
     <div className={className}>
       <Header
@@ -27,6 +61,12 @@ function Blog() {
         zenMode={zenMode}
         toggleZenMode={toggleZenMode}
       />
+      { loading && <Spinner /> }
+
+      <button onClick={loadPosts}>Charger les posts</button>
+
+      { !loading
+      && (
       <Routes>
         {
           categoriesData.map(
@@ -34,14 +74,15 @@ function Blog() {
               <Route
                 key={categorie.label}
                 path={categorie.route}
-                element={<Posts posts={filterPostByCategory(postsData, categorie.label)} />}
+                element={<Posts posts={filterPostsByCategory(posts, categorie.label)} />}
               />
             ),
           )
         }
-        <Route path="*" element={<div>Page not found...</div>} />
+        <Route path="jquery" element={<Navigate to="/autre" />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-
+      )}
       <Footer />
     </div>
   );
